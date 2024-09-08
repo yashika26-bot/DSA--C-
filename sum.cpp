8d62468e1bed72583ac39c5749805de8605b59d3
Yashika sharma <ys5326706@gmail.com>
2024-09-08 11:08:07 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;  //Taking input from user
+    cout<<"Enter value of a :";
+    cin>>a;
+
+    cout<<"Enter value of b:";
+    cin>>b;
+
+    int sum = a+b;
+    cout<<"the sum of no. is:"<<sum;
+    
+    
+    
+// SWAP NUMBERS 
+int c,d;
+cout<<"c:";
+cin>>c;
+cout<<"d:";
+cin>>d;
+
+int y;
+y=d;
+d=c;
+c=y;
+
+return 0;
+}
+
