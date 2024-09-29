2240ba487c7b12ed5266eb436a9f1d61a486e3ff
Yashika sharma <ys5326706@gmail.com>
2024-09-29 17:44:16 +0530

    Add files via upload

---
+++ Absolutevalue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a;
+    cout<<"Enter an integer :-"<<endl;
+    cin>>a;
+
+    int AbsoluteValue = abs(a);
+    cout<<"Absolute value of a = "<<AbsoluteValue<<endl;
+
+    return 0;
+
+}
